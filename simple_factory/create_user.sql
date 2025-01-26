19651219e0e5bd867ba2a02a70efb83c9997b2a6
Thomas Weise <tweise@hfuu.edu.cn>
2025-01-26 10:12:33 +0800

    modified script

--- simple_factory/create_user.sql
+++ simple_factory/create_user.sql
@@ -1,9 +1,5 @@
 /* In this example, we create a new user for our database. */
 
--- If the user 'boss' already exists, we delete it.
--- This allows you to run the example several times without error.
-DROP USER IF EXISTS boss;
-
 -- On PostgreSQL, there is a table `pg_catalog.pg_user` with all users.
 -- We print the column `usename` with the user names.
 SELECT usename FROM pg_catalog.pg_user;
