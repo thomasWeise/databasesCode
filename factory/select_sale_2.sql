ccc0d9b28bd5b13df55d4e421b3a02379f2ff4c1
Thomas Weise <tweise@hfuu.edu.cn>
2025-02-01 13:49:54 +0800

    repeated select sales example

---
+++ factory/select_sale_2.sql
@@ -0,0 +1,10 @@
+/* Extract again information from our database using the view 'sale'. */
+
+-- Get the total sales per customer again.
+SELECT customer, SUM(amount * price) AS per_customer_sale
+    FROM sale GROUP BY customer ORDER BY customer;
+
+-- Get the total sales per product again.
+SELECT product, SUM(amount) AS amount,
+    SUM(amount * price) AS per_product_sale
+    FROM sale GROUP BY product ORDER BY product;
