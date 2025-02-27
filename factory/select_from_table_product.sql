354b899c4f302af6ac06fb58bbb269698d8490fb
Thomas Weise <tweise@hfuu.edu.cn>
2025-02-27 13:09:26 +0800

    changed to gram per yuan

--- factory/select_from_table_product.sql
+++ factory/select_from_table_product.sql
@@ -3,9 +3,9 @@
 -- List the names and prices of all purses.
 SELECT name, price from product WHERE name LIKE '%Purse%';
 
--- Order anything that is not a shoe in terms of cost per gram.
-SELECT name, width*height*depth/(1000*price) as cm³_per_yuan from product
-  ORDER BY cm³_per_yuan DESC LIMIT 5;
+-- Order anything that is not a shoe in terms of grams per yuan.
+SELECT name, weight / price as g_per_yuan from product
+  ORDER BY g_per_yuan DESC LIMIT 5;
 
 -- Get the average price per product type.
 SELECT type, AVG(price) FROM
