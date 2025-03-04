782b6a3dda646cf8171d789f1762d2f02f1ab669
Thomas Weise <tweise@hfuu.edu.cn>
2025-03-04 09:03:57 +0800

    fixed naming

--- factory/select_from_view_sale_2.sql
+++ factory/select_from_view_sale_2.sql
@@ -1,12 +1,12 @@
 /* Extract some information from our database using the view 'sale'. */
 
 -- Get the total sales per customer in 2025.
-SELECT customer_name, SUM(amount * price) AS per_customer_sale FROM sale
+SELECT customer_name, SUM(amount * price) AS customer_sale FROM sale
     WHERE (ordered >= '2025-01-01') AND (ordered < '2026-01-01')
-    GROUP BY customer_name ORDER BY per_customer_sale DESC;
+    GROUP BY customer_name ORDER BY customer_sale DESC;
 
 -- Get the total sales per product in 2025.
 SELECT product_name, SUM(amount) AS total_amount,
-    SUM(amount * price) AS per_product_sale FROM sale
+    SUM(amount * price) AS product_sale FROM sale
     WHERE (ordered >= '2025-01-01') AND (ordered < '2026-01-01')
-    GROUP BY product_name ORDER BY per_product_sale DESC;
+    GROUP BY product_name ORDER BY product_sale DESC;
--- factory/select_from_view_sale_3.sql
+++ factory/select_from_view_sale_3.sql
@@ -1,10 +1,10 @@
 /* Extract again information from our database using the view 'sale'. */
 
 -- Get the total sales per customer.
-SELECT customer_name, SUM(amount * price) AS per_customer_sale FROM sale
-    GROUP BY customer_name ORDER BY per_customer_sale DESC;
+SELECT customer_name, SUM(amount * price) AS customer_sale FROM sale
+    GROUP BY customer_name ORDER BY customer_sale DESC;
 
 -- Get the total sales per product.
 SELECT product_name, SUM(amount) AS total_amount,
-    SUM(amount * price) AS per_product_sale FROM sale
-    GROUP BY product_name ORDER BY per_product_sale DESC;
+    SUM(amount * price) AS product_sale FROM sale
+    GROUP BY product_name ORDER BY product_sale DESC;
