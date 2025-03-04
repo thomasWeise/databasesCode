1c30b23a07cff447d4b7da9fbb6ca091c078afa7
Thomas Weise <tweise@hfuu.edu.cn>
2025-03-04 09:02:26 +0800

    renamed amount for clarity

--- factory/select_from_view_sale_2.sql
+++ factory/select_from_view_sale_2.sql
@@ -6,7 +6,7 @@ SELECT customer_name, SUM(amount * price) AS per_customer_sale FROM sale
     GROUP BY customer_name ORDER BY per_customer_sale DESC;
 
 -- Get the total sales per product in 2025.
-SELECT product_name, SUM(amount) AS amount,
+SELECT product_name, SUM(amount) AS total_amount,
     SUM(amount * price) AS per_product_sale FROM sale
     WHERE (ordered >= '2025-01-01') AND (ordered < '2026-01-01')
     GROUP BY product_name ORDER BY per_product_sale DESC;
--- factory/select_from_view_sale_3.sql
+++ factory/select_from_view_sale_3.sql
@@ -5,6 +5,6 @@ SELECT customer_name, SUM(amount * price) AS per_customer_sale FROM sale
     GROUP BY customer_name ORDER BY per_customer_sale DESC;
 
 -- Get the total sales per product.
-SELECT product_name, SUM(amount) AS amount,
+SELECT product_name, SUM(amount) AS total_amount,
     SUM(amount * price) AS per_product_sale FROM sale
     GROUP BY product_name ORDER BY per_product_sale DESC;
