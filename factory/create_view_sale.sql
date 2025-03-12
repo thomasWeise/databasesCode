8caffe101bc3fb9162f826238732aacb28f5b47f
Thomas Weise <tweise@hfuu.edu.cn>
2025-03-12 11:47:08 +0800

    fixed view sale to use unique customer names

--- factory/create_view_sale.sql
+++ factory/create_view_sale.sql
@@ -5,14 +5,15 @@
 -- in the 'customer' and 'product' tables (via the INNER JOIN).
 -- This gives us one long row with all the information for one 'demand'.
 -- We now choose only some elements of this long row and rename them.
--- We extract the name of the customer and refer to it "customer_name".
+-- We extract the name of the customer combined with their phone number
+-- and refer to it "customer_name".
 -- We extract the name of the product and refer to it as "product_name".
 -- We also print the "amount" from each customer demand and the price.
 -- We also print when the purchase as made
 CREATE VIEW sale AS
-    SELECT customer.name AS customer_name, product.name AS product_name,
-           product.price AS price, demand.amount AS amount,
-           demand.ordered as ordered
+    SELECT customer.name || ', ' || customer.phone AS customer_name,
+           product.name AS product_name, product.price AS price,
+           demand.amount AS amount, demand.ordered as ordered
     FROM demand INNER JOIN customer ON (customer.id = demand.customer)
                 INNER JOIN product ON (product.id = demand.product)
     ORDER BY customer_name, ordered, product_name, price, amount;
--- factory/update_according_to_base_example.sql
+++ factory/update_according_to_base_example.sql
@@ -2,4 +2,5 @@
 
 -- Insert 1 order into our demand table.
 INSERT INTO demand (customer, product, amount, ordered)
-VALUES (4,  7, 11, '2025-04-12');
+VALUES (4, 7, 11, '2025-04-12'),  -- manual insertion
+       (1, 4,  7, '2025-05-07');  -- inserted via form
