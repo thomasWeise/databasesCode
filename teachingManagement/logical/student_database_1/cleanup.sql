cfdc36f5a36d8f161e340a6b7c94ab45dbe01c9f
Thomas Weise <tweise@hfuu.edu.cn>
2025-04-18 13:50:29 +0800

    slight modification

--- teachingManagement/logical/student_database_1/cleanup.sql
+++ teachingManagement/logical/student_database_1/cleanup.sql
@@ -1,3 +1,3 @@
-/* Cleanup after the example: Delete the student database with its tables. */
+/* Cleanup after the example: Delete the student database. */
 
 DROP DATABASE IF EXISTS student_database;
