6d91aea1309cf9265984d742a27d0b459ea19197
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-01 20:31:45 +0800

    better documentation

--- teachingManagement/logical/person_database_2/select.sql
+++ teachingManagement/logical/person_database_2/select.sql
@@ -6,6 +6,6 @@ SELECT full_name || ': ' || country || '-' || province || ', ' ||
     INNER JOIN person  ON person.id = name.person
     INNER JOIN address ON person.id = address.person
     INNER JOIN has_id  ON person.id = has_id.person
-    WHERE name.is_official = TRUE AND
-          has_id.id_type   = 2
-    ORDER BY person.date_of_birth;
+    WHERE name.is_official = TRUE AND  -- use official name only
+          has_id.id_type   = 2      -- use ID type for mobile phone
+    ORDER BY person.date_of_birth;  -- sort by age, oldest people first
