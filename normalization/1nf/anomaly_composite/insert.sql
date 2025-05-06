e03916c409d43bc2fa242d77e20e61c9bfd43290
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-06 11:16:03 +0800

    removed chinese characters ... i cannot get them into listings

--- normalization/1nf/anomaly_composite/insert.sql
+++ normalization/1nf/anomaly_composite/insert.sql
@@ -5,9 +5,8 @@ INSERT INTO address (address) VALUES
     ('Hefei University, Hefei 230601 Jinkaiqu, Hefei, Anhui, China'),
     ('Am Rathaus 1, 09111 Zentrum, Chemnitz, Sachsen, Deutschland'),
     ('Canal Street 4, Chinatown, New York, NY, USA'),
-    ('West Street, Licheng District, Quanzhou 362002, Fujian, PRC'),
-    ('合肥大学南艳湖校区 中国安徽省合肥市230601蜀山区经济技术开发区锦绣大道99号');
+    ('West Street, Licheng District, Quanzhou 362002, Fujian, PRC');
 
 -- Create the student records.
 INSERT INTO student (name, address) VALUES
-    ('Bibbo', 1), ('Bebbo', 2), ('Bibbi', 3), ('Babbo', 4), ('Bobbo', 5);
+    ('Bibbo', 1), ('Bebbo', 2), ('Bibbi', 3), ('Babbo', 4);
