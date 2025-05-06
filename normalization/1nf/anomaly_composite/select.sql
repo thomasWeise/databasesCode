859bcb300455fe9aef31497a4b68273c354b2521
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-06 14:42:19 +0800

    slight improvement

--- normalization/1nf/anomaly_composite/select.sql
+++ normalization/1nf/anomaly_composite/select.sql
@@ -2,14 +2,14 @@
 
 -- Fails to get addresses from the PRC and includes addresses from
 -- Chinatown, New York.
-SELECT name, full_address FROM student
+SELECT name, full_address as address_attempt_1 FROM student
     INNER JOIN address ON student.address = address.id
     WHERE full_address ILIKE '%china%';
 
 -- Gets everything right, but is still error prone.
 -- For examples, what if China was written in Chinese?
 -- What with other words or names that include China?
-SELECT name, full_address FROM student
+SELECT name, full_address as address_attempt_2 FROM student
     INNER JOIN address ON student.address = address.id
     WHERE ((full_address ILIKE '%china%') AND NOT
            (full_address ILIKE '%chinatown%'))
