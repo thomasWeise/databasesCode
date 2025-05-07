75f00824707b1855b11572969961cf5d675875d5
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-08 06:04:27 +0800

    slight streamlining

--- normalization/1nf/anomaly_multivalued/select.sql
+++ normalization/1nf/anomaly_multivalued/select.sql
@@ -8,8 +8,7 @@ UNION SELECT id AS sid, name, address_2 AS adr FROM student
 -- Use the above as *subquery* to construct the overall result.
 SELECT name, city || ', ' || street_address AS address FROM (
         SELECT       id AS sid, name, address_1 AS adr FROM student
-        UNION SELECT id AS sid, name, address_2 AS adr FROM student
-            WHERE address_2 IS NOT NULL)
+        UNION SELECT id AS sid, name, address_2 AS adr FROM student)
     INNER JOIN address ON adr = address.id
     WHERE country ILIKE ANY(ARRAY['%china%', '%PRC%', '%P.R.C.%']);
 
@@ -17,7 +16,6 @@ SELECT name, city || ', ' || street_address AS address FROM (
 SELECT DISTINCT ON (sid)
     name, city || ', ' || street_address AS address FROM (
         SELECT       id AS sid, name, address_1 AS adr FROM student
-        UNION SELECT id AS sid, name, address_2 AS adr FROM student
-            WHERE address_2 IS NOT NULL)
+        UNION SELECT id AS sid, name, address_2 AS adr FROM student)
     INNER JOIN address ON adr = address.id
     WHERE country ILIKE ANY(ARRAY['%china%', '%PRC%', '%P.R.C.%']);
