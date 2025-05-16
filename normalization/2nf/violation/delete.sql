8413920e26b7385efd47f1158c5623e28a0fa56d
Thomas Weise <tweise@hfuu.edu.cn>
2025-05-16 10:48:26 +0800

    slight improvement

--- normalization/2nf/violation/delete.sql
+++ normalization/2nf/violation/delete.sql
@@ -2,4 +2,5 @@
 
 -- Delete rooms 904a and 904b in building 53.
 DELETE FROM building_room WHERE building_number = '53'
-    AND room_number LIKE '904%';
+    AND room_number LIKE '904%'
+    RETURNING building_number, room_number;
