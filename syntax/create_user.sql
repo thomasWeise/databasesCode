edf065ad45543e8b53ecd38d99b6ab4f366a877e
Thomas Weise <tweise@hfuu.edu.cn>
2025-06-30 12:57:40 +0800

    first syntax example

---
+++ syntax/create_user.sql
@@ -0,0 +1,4 @@
+-- The syntax of the "CREATE USER" command:
+-- 'username' is the name of the user that we want to create.
+-- 'password' is the password that we want to assign to this user.
+CREATE USER username WITH WITH ENCRYPTED PASSWORD 'password';
