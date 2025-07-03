8a4352b751083b5d4557e293d186d48105714c4b
Thomas Weise <tweise@hfuu.edu.cn>
2025-07-04 07:55:33 +0800

    added example for domestic customers

--- factory/select_from_table_customer.sql
+++ factory/select_from_table_customer.sql
@@ -3,6 +3,9 @@
 -- Try to find customers who may be living in France.
 SELECT name, address FROM customer WHERE address LIKE '%France%';
 
+-- Which customers are domestic, i.e., live in China?
+SELECT name, address ILIKE '%china%' as domestic FROM customer;
+
 -- Count how many domestic and foreign customers we have.
 SELECT COUNT(*), address ILIKE '%china%' as domestic FROM customer
     GROUP BY domestic;
