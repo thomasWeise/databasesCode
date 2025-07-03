c40f14b5b2ddd9997ba05f677a75bc8b673d04d3
Thomas Weise <tweise@hfuu.edu.cn>
2025-07-03 16:53:42 +0800

    added syntax for constraint

--- syntax/create_table_constraint.sql
+++ syntax/create_table_constraint.sql
@@ -1,4 +1,4 @@
--- Create a New Table.
+-- Create a New Table with additional Constraints.
 --
 --            tableName: the name of the table to create
 --              columnX: the name of the X-th column.
@@ -6,9 +6,18 @@
 -- constraintsOnColumnX: constraints imposed on the X-th column, can be
 --                       omitted or a combination of UNIQUE, NOT NULL,
 --                       PRIMARY KEY, etc.
+--      constraintNameY: the name of the Y-th (check) constraint
+--          expressionY: the expression of the Y-th (check) constraint
+--
+-- Everytime a row is entered into the table or changed, the expressions
+-- of all constraints are checked. If one of them is not TRUE, then the
+-- operation is cancelled, not performed, and an error is reported.
 CREATE TABLE tableName (
     column1 typeOfColumn1 constraintsOnColumn1,
     column2 typeOfColumn2 constraintsOnColumn2,
     column3 typeOfColumn3 constraintsOnColumn3,
+    ...
+    CONSTRAINT constraintName1 CHECK expression1,
+    CONSTRAINT constraintName2 CHECK expression2,
     ...
 );
