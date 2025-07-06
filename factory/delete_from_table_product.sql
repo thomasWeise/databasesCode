749f824147dbecca2e0d91e5e47d176e335226ce
Thomas Weise <tweise@hfuu.edu.cn>
2025-07-06 19:47:35 +0800

    added returning clause

--- factory/delete_from_table_product.sql
+++ factory/delete_from_table_product.sql
@@ -4,7 +4,8 @@
 SELECT COUNT(*) as number_of_products from product;
 
 -- Delete the 'Shoe, Size 36' ... nobody ever bought it.
-DELETE FROM product WHERE id = 1;  -- The id of 'Shoe, Size 36' is 1.
+DELETE FROM product WHERE id = 1  -- The id of 'Shoe, Size 36' is 1.
+RETURNING id, name;  -- get the id and name of the deleted row.
 
 -- We now we got only 10 products.
 SELECT COUNT(*) as number_of_products from product;
--- syntax/delete.sql
+++ syntax/delete.sql
@@ -1,3 +1,6 @@
 -- Delete all records from table 'tableName' that meet the 'WHERE'
 -- condition.
-DELETE FROM tableName WHERE <condition>;
+-- Some DBMSes allow to use a RETURNING clause, which acts like a
+-- SELECT applied *only* to the deleted rows (before they are purged).
+DELETE FROM tableName WHERE <condition>
+RETURNING column1, column2, ...;
