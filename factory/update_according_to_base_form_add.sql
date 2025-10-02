36a2b4b71526424ab94ebedc14f1d801f62d53a4
Thomas Weise <tweise@hfuu.edu.cn>
2025-10-03 05:40:07 +0800

    added returning

--- factory/update_according_to_base_form_add.sql
+++ factory/update_according_to_base_form_add.sql
@@ -3,4 +3,5 @@
 -- Insert 1 order into our demand table in the same way we did using the
 -- form in LibreOffice Base.
 INSERT INTO demand (customer, product, amount, ordered)
-VALUES (1, 4,  7, '2025-05-07');  -- inserted via form
+    VALUES (1, 4,  7, '2025-05-07')
+    RETURNING *;
--- factory/update_according_to_base_table_add.sql
+++ factory/update_according_to_base_table_add.sql
@@ -3,4 +3,5 @@
 -- Insert 1 order into our demand table, in the same way we manually did.
 -- using the table view in LibreOffice Base.
 INSERT INTO demand (customer, product, amount, ordered)
-VALUES (4, 7, 11, '2025-04-12');  -- manual insertion
+    VALUES (4, 7, 11, '2025-04-12')
+    RETURNING *;
