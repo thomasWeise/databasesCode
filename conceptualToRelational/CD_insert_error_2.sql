8852eb95bf185dc8d71615905f624c8839feb34f
Thomas Weise <tweise@hfuu.edu.cn>
2025-11-18 11:17:02 +0800

    fixed example

--- conceptualToRelational/CD_insert_error_2.sql
+++ conceptualToRelational/CD_insert_error_2.sql
@@ -1,4 +1,4 @@
 /* Insert a wrong row into tables for C-|o-----||-D relationship. */
 
 -- It is impossible to create a row in C that references no row in D.
-INSERT INTO c (x) VALUES (3, '365');
+INSERT INTO c (x) VALUES ('365');
