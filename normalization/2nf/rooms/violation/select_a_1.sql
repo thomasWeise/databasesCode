8b7b942fe6d31c9ec3c6a702e5116da9fb621fd1
Thomas Weise <tweise@hfuu.edu.cn>
2025-11-28 05:56:30 +0800

    split example

---
+++ normalization/2nf/rooms/violation/select_a_1.sql
@@ -0,0 +1,6 @@
+/** Find the all rooms available in South Campus 2. */
+
+-- Several rooms are missing, because the data is inconsistent:
+-- Their address was 'South Campus II'.
+SELECT building_number, room_number FROM building_room
+    WHERE address = 'South Campus 2';
---
+++ normalization/2nf/rooms/violation/select_a_2.sql
@@ -0,0 +1,10 @@
+/** Find the all rooms available in South Campus 2. */
+
+-- Several rooms are missing, because the data is inconsistent:
+-- Their address was 'South Campus II'.
+SELECT building_number, room_number FROM building_room
+    WHERE address = 'South Campus 2';
+
+-- Now we get these rooms.
+SELECT building_number, room_number FROM building_room
+    WHERE address = 'South Campus 2' OR address = 'South Campus II';
