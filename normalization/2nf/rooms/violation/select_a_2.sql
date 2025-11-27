5a7c2f3208025a9f6154b0c9e8376ba9caf16279
Thomas Weise <tweise@hfuu.edu.cn>
2025-11-28 06:33:12 +0800

    minor fixes

--- normalization/2nf/rooms/violation/select_a_2.sql
+++ normalization/2nf/rooms/violation/select_a_2.sql
@@ -1,4 +1,4 @@
-/** Find the all rooms available in South Campus 2. */
+/** Find all the rooms available in South Campus 2. */
 
 -- Several rooms are missing, because the data is inconsistent:
 -- Their address was 'South Campus II'.
--- normalization/3nf/student/fixed/select.sql
+++ normalization/3nf/student/fixed/select.sql
@@ -1,4 +1,4 @@
-/** Get the names of all parents of the all students. */
+/** Get the names of all parents of all the students. */
 
 SELECT student_name, parent_name FROM student
     INNER JOIN parent ON student.parent_mobile = parent.parent_mobile;
