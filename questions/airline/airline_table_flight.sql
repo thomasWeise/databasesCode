06c9567bf82d07b1c108df5d000c6e844b935ba6
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-02 11:29:39 +0800

    added flight date

--- questions/airline/airline_table_flight.sql
+++ questions/airline/airline_table_flight.sql
@@ -1,11 +1,11 @@
 -- Create the flight table of the airline example.
 CREATE TABLE flight (
     flight_number    VARCHAR(8)    NOT NULL PRIMARY KEY,
-    start_airport    VARCHAR(255)  NOT NULL,
+    start_airport    VARCHAR(20)   NOT NULL,
     start_time       TIME          NOT NULL,
-    dest_airport     VARCHAR(255)  NOT NULL,
+    dest_airport     VARCHAR(20)   NOT NULL,
     duration         INTERVAL      NOT NULL,
-    CONSTRAINT duration_ok   CHECK (duration > '10 minutes'),
+    CONSTRAINT duration_ok   CHECK (duration      > '10 minutes'),
     CONSTRAINT flight_num_ok CHECK (flight_number ~ '^[A-Z]+\d{1,5}$'),
     CONSTRAINT airports_ok   CHECK (dest_airport != start_airport)
 );
