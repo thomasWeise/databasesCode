5818845b94dfd59532a0891cfa06683c347cf08e
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-02 19:52:08 +0800

    fixed hefei abbreviation

--- questions/airline/airline_insert_flight.sql
+++ questions/airline/airline_insert_flight.sql
@@ -1,10 +1,10 @@
 -- Insert into the flight table.
 INSERT INTO flight (flight_number, start_airport, start_time,
                     dest_airport, duration)
-VALUES ('XA1843', 'PEK', '13:30', 'HEF',  '1 hour  55 minutes'),
+VALUES ('XA1843', 'PEK', '13:30', 'HFE',  '1 hour  55 minutes'),
        ('XA1844', 'HFE', '16:15', 'PEK',  '2 hours'),
-       ('XA1813', 'PEK', '07:55', 'HEF',  '1 hour  55 minutes'),
-       ('XA1814', 'HEF', '10:45', 'PEK',  '2 hours 15 minutes'),
+       ('XA1813', 'PEK', '07:55', 'HFE',  '1 hour  55 minutes'),
+       ('XA1814', 'HFE', '10:45', 'PEK',  '2 hours 15 minutes'),
        ('XU489',  'PEK', '03:20', 'BER',  '9 hours 10 minutes'),
        ('XU490',  'BER', '12:55', 'PEK', '10 hours 20 minutes')
 RETURNING *;
