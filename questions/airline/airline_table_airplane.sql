7f02aa92bba57a6163110c2d718ae052e576f1ea
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-02 09:45:43 +0800

    added some gimmicks

--- questions/airline/airline_table_airplane.sql
+++ questions/airline/airline_table_airplane.sql
@@ -2,4 +2,5 @@
 CREATE TABLE airplane (
     tail_number   VARCHAR(10) NOT NULL UNIQUE PRIMARY KEY,
     airplane_type INT         NOT NULL REFERENCES airplane_type (id)
+    CONSTRAINT tail_number_ok CHECK (tail_number ~ '^[A-Z]+-\d{2,8}$')
 );
