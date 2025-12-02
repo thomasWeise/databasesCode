c5d0584586b30e728191b904e5339c7db4d75ce3
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-02 12:48:31 +0800

    minor fix 14

--- questions/airline/airline_join_02.sql
+++ questions/airline/airline_join_02.sql
@@ -3,5 +3,5 @@ SELECT passenger.name AS name, flight.start_airport, flight.dest_airport,
     FROM flight_date
     INNER JOIN flight    ON flight.flight_number == flight_date.flight
     INNER JOIN booking   ON booking.flight_date  == flight_date.id
-    INNER JOIN passenger ON passenger.id         == booking.passender
+    INNER JOIN passenger ON passenger.id         == booking.passenger
     WHERE passenger.name ILIKE '%Pepp%';
