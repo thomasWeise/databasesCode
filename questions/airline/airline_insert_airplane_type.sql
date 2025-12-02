cbea95bb49f102798f14cee0ae35344b7fae82e4
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-02 08:40:12 +0800

    minor fix 3

--- questions/airline/airline_insert_airplane_type.sql
+++ questions/airline/airline_insert_airplane_type.sql
@@ -2,4 +2,4 @@
 INSERT INTO airplane_type (name, speed) VALUES
     ('Junkers F 13', 170),          ('Douglas DC-3', 370),
     ('Messerschmitt Me 262', 870),  ('Airbus A320', 962)
-) RETURNING *;
+RETURNING *;
