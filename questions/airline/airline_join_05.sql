65426e98ebd1b5bf7061a795b8a0d74799c33def
Thomas Weise <tweise@hfuu.edu.cn>
2025-12-03 10:09:16 +0800

    minor simplifications

--- questions/airline/airline_join_05.sql
+++ questions/airline/airline_join_05.sql
@@ -1,6 +1,5 @@
 /* Merge all the data from all the tables */
-SELECT flight_number, start_date, start_time, start_airport, duration, dest_airport, tail_number, airplane_type.name AS type_name, speed,
- pilot.name AS pilot, flight_hours, qualification, pilot.date_of_birth AS pilot_dob, passenger.name AS passenger, passenger.date_of_birth AS passenger_dob, address, class, price
+SELECT flight_number, start_date AS date, start_time AS time, start_airport AS start, duration, dest_airport AS dest, tail_number, airplane_type.name AS type, speed, pilot.name AS pilot, flight_hours AS hrs, qualification AS qual, pilot.date_of_birth AS pilot_dob, passenger.name AS passenger, passenger.date_of_birth AS passenger_dob, address, class, price
 FROM booking
 INNER JOIN flight_date   ON booking.flight_date  = flight_date.id
 INNER JOIN passenger     ON passenger.id         = booking.passenger
